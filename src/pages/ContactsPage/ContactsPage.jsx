b72b077b07d364cc49b57e8aded37f00daaa2408
Serhii-Yakimtso <serafyso@gmail.com>
2024-05-23 00:11:38 +0300

    repair ContactsPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,26 +2,26 @@
 // контактів користувача, на якому рендериться
 // компонент сторінки ContactsPage.
 
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import ContactList from '../../components/ContactList/ContactList';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import SearchBox from '../../components/SearchBox/SearchBox';
 // import TaskEditor from '../../components/TaskEditor/TaskEditor';
-// import { fetchTasks } from '../../redux/tasks/operations';
-// import { selectLoading } from '../../redux/tasks/selectors';
+import { fetchContacts } from '../../redux/contacts/operations';
+import { selectLoading, selectError } from '../../redux/contacts/selectors';
 // import { selectError } from '../../redux/tasks/selectors';
 
 export default function TasksPage() {
-  //   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  //   const isLoading = useSelector(selectLoading);
-  //   const isError = useSelector(selectError);
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
 
-  //   useEffect(() => {
-  //     dispatch(fetchContacts());
-  //   }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <>
